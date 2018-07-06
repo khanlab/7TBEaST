4d790f1daa1161c2ef566a998dfe382ffebcd6b2
Martybird <xym6325@gmail.com>
2018-07-05 21:29:07 -0400

    Update 7TBEAST

--- testmultiprocess.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --reservation coss-wr_cpu
-#SBATCH --account coss-wa
-#SBATCH –-mem-per-cpu=4G
-#SBATCH –-time=0-00:10
-#SBATCH –-cpus-per-task=4
-time python euler39_multiprocess.py
